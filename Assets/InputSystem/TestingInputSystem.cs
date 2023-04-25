8de4e882074b762cf706173f8613e40692b96b5a
ardmos <46891528+ardmos@users.noreply.github.com>
2023-04-25 20:36:25 +0900

    Feat: InputSystem의 'InvokeC# Events'를 사용한 점프 구현 방식 추가

--- Assets/InputSystem/TestingInputSystem.cs
+++ Assets/InputSystem/TestingInputSystem.cs
@@ -1,18 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+
 
 public class TestingInputSystem : MonoBehaviour
 {
     private Rigidbody playerRigidbody;
+    private PlayerInput playerInput;
 
     private void Awake()
     {
-        playerRigidbody = GetComponent<Rigidbody>();    
+        playerRigidbody = GetComponent<Rigidbody>();
+        playerInput = GetComponent<PlayerInput>();
+
+        playerInput.onActionTriggered += PlayerInput_onActionTriggered;
     }
 
-    public void Jump() {
-        Debug.Log("Jump!");
-        playerRigidbody.AddForce(Vector3.up * 5f, ForceMode.Impulse);
+    private void PlayerInput_onActionTriggered(InputAction.CallbackContext context)
+    {
+        Debug.Log(context);
+    }
+
+
+    public void Jump(InputAction.CallbackContext context) {
+        if (context.performed)
+        {
+            Debug.Log("Jump! " + context.phase);
+            playerRigidbody.AddForce(Vector3.up * 5f, ForceMode.Impulse);
+        }
     }
 }
