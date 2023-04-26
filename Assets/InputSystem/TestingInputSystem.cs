7d7da3f2e589788b4dd947801cba971101fe293b
ardmos <46891528+ardmos@users.noreply.github.com>
2023-04-26 18:43:32 +0900

    Feat: InputSystem C#파일 생성 후 활용 방식 테스트 및 적용

--- Assets/InputSystem/TestingInputSystem.cs
+++ Assets/InputSystem/TestingInputSystem.cs
@@ -7,22 +7,16 @@ using UnityEngine.InputSystem;
 public class TestingInputSystem : MonoBehaviour
 {
     private Rigidbody playerRigidbody;
-    private PlayerInput playerInput;
 
     private void Awake()
     {
         playerRigidbody = GetComponent<Rigidbody>();
-        playerInput = GetComponent<PlayerInput>();
 
-        //////// 여기부터!!  영상 16:00 부터 보면 됨.  아래 줄 제거 후 진행해도 무방해보임
-        playerInput.onActionTriggered += PlayerInput_onActionTriggered;
+        PlayerInputActions playerInputActions = new PlayerInputActions();
+        playerInputActions.Player.Enable();
+        playerInputActions.Player.Jump.performed += Jump;
+        
     }
-
-    private void PlayerInput_onActionTriggered(InputAction.CallbackContext context)
-    {
-        Debug.Log(context);
-    }
-
 
     public void Jump(InputAction.CallbackContext context) {
         Debug.Log(context);
