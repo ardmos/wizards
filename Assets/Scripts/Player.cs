91c7da914c8be113963aede890e2c5cb260b3efe
ardmos <46891528+ardmos@users.noreply.github.com>
2023-05-04 23:50:49 +0900

    Fix: InputSystem Refactor ( legacy -> new )
    기존 WASD 인식에서
    뉴 인풋시스템 사용. WASD, 방향키, 모바일 패드 인식 가능하도록 변경

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 7f;
+    [SerializeField] private GameInput gameInput;
+    private bool isWalking;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
+
+        Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
+        transform.position += moveDir * moveSpeed * Time.deltaTime;
+        isWalking = moveDir != Vector3.zero;
+
+        Rotate(moveDir);
+    }
+
+    private void Rotate(Vector3 mMoveDir)
+    {
+        float rotateSpeed = 10f;
+        Vector3 slerpResult = Vector3.Slerp(transform.forward, mMoveDir, Time.deltaTime * rotateSpeed);       
+        transform.forward = slerpResult;
+    }
+
+    public bool IsWalking()
+    {
+        return isWalking;
+    }
+}
--- Assets/Scripts/PlayerAnimator.cs
+++ Assets/Scripts/PlayerAnimator.cs
@@ -6,7 +6,7 @@ public class PlayerAnimator : MonoBehaviour
 {
     private const string IS_WALKING = "IsWalking";
 
-    [SerializeField] private PlayerController playerController;
+    [SerializeField] private Player player;
     private Animator animator;
 
     private void Awake()
@@ -16,7 +16,7 @@ public class PlayerAnimator : MonoBehaviour
 
     private void Update()
     {
-        animator.SetBool(IS_WALKING, playerController.IsWalking());
+        animator.SetBool(IS_WALKING, player.IsWalking());
     }
 
 
