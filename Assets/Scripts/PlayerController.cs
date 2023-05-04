e7fef77a17887f4782ec058792223d41783d72a1
ardmos <46891528+ardmos@users.noreply.github.com>
2023-05-04 23:01:16 +0900

    Feat: 플레이어 캐릭터 걷기 애니메이션 추가.
    PlayerController.cs 와 PlayerAnimator.cs 를 구분해서 걷기 애니메이션 재생 구현

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float moveSpeed = 7f;
+    [SerializeField] private float moveSpeed = 7f;
+    private bool isWalking;
 
 
     // Update is called once per frame
@@ -38,6 +39,7 @@ public class PlayerController : MonoBehaviour
 
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
         transform.position += moveDir * moveSpeed * Time.deltaTime;
+        isWalking = moveDir != Vector3.zero;
 
         Rotate(moveDir);
     }
@@ -47,5 +49,10 @@ public class PlayerController : MonoBehaviour
         float rotateSpeed = 10f;
         Vector3 slerpResult = Vector3.Slerp(transform.forward, mMoveDir, Time.deltaTime * rotateSpeed);       
         transform.forward = slerpResult;
+    }
+
+    public bool IsWalking()
+    {
+        return isWalking;
     }
 }
