e795b11f0148beb2712b072b939ef4f7b7d7b1f7
ardmos <46891528+ardmos@users.noreply.github.com>
2023-09-26 18:00:19 +0900

    feat: 마법상점 활성/비활성화 영역 콜라이더 스크립트 추가

---
+++ Assets/Scripts/Game/Store/StoreTriggerAreaCollider.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// 플레이어의 상점 접근 여부를 감지해서 상점을 활성/비활성 시키는 스크립트
+/// !!!현재 기능
+///     1. 플레이어 접근시 손님으로 등록
+///     2. 플레이어 접근시 상점 팝업 활성화
+///     3. 플레이어 이탈시 상점 팝업 비활성화
+/// </summary>
+public class StoreTriggerAreaCollider : MonoBehaviour
+{
+    [SerializeField] private UI_MagicStore uiMagicStore;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        IStoreCustomer storeCustomer = other.GetComponent<IStoreCustomer>();
+        if(storeCustomer != null)
+        {
+            uiMagicStore.Show(storeCustomer);
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        IStoreCustomer storeCustomer = other.GetComponent<IStoreCustomer>();
+        if (storeCustomer != null)
+        {
+            uiMagicStore.Hide();
+        }
+    }
+}
