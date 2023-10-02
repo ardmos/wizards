b557f8523ef4131b439e93f9311968ef55874416
ardmos <46891528+ardmos@users.noreply.github.com>
2023-10-02 19:47:35 +0900

    feat: 여러 Spell들 교체를 전략패턴으로 구현하기 위한 준비.
    중간에 스킬 발동 입력 처리에 inputsystem을 사용하기 위해서 추가 작업을 진행할 것이다

---
+++ Assets/Scripts/Data/SpellData.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct SpellData
+{
+    public float coolTime;
+    public int damage;
+    public string spellName;
+    public GameObject spellObject;
+}
