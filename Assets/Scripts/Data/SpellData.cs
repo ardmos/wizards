1770f3e8b7bec9027aa79cf042853d76ae070743
ardmos <46891528+ardmos@users.noreply.github.com>
2023-10-04 01:32:35 +0900

    feat: Spell시스템 구현중2

--- Assets/Scripts/Data/SpellData.cs
+++ Assets/Scripts/Data/SpellData.cs
@@ -2,7 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public struct SpellData
+/// <summary>
+/// 마법의 공통적인 속성들을 관리하는 클래스
+/// </summary>
+public class SpellData : MonoBehaviour
 {
     public enum SpellType
     {
@@ -12,12 +15,31 @@ public struct SpellData
         Lightning,
         Arcane
     }
+    public SpellType spellType;
 
     public float coolTime;
+    public float restTime;
     public float lifeTime;
     public float moveSpeed;
     public int damage;
     public int price;
+    public int level;
     public string spellName;
-    public GameObject spellObject;
+    public bool castAble;
+
+    public GameObject spellObjectPref;
+
+    public SpellData()
+    {
+        coolTime = 0;
+        restTime = 0;
+        lifeTime = 0;
+        moveSpeed = 0;
+        damage = 0;
+        price = 0;
+        level = 0;
+        spellName = "";
+        castAble = true;
+        spellObjectPref = null;
+    }
 }
--- Assets/Scripts/Game/Spell.cs
+++ Assets/Scripts/Game/Spell.cs
@@ -4,18 +4,18 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 
 /// <summary>
-/// 마법의 공통적인 속성들을 가진 클래스
+/// 마법의 공통적인 기능들을 관리하는 클래스
 /// </summary>
 public abstract class Spell : MonoBehaviour
 {
-    public SpellData data;
-    bool castAble = true;
-    float restTime = 0;
+    public SpellData spellData;
 
     /// <summary>
-    /// 각 스펠의 디테일값은 스펠 본인들이 상속받아서 정의
+    /// 각 스펠의 디테일값(spellData)은 스펠 본인들이 상속받아서 정의
     /// </summary>
-    public abstract void InitSpellDetail();
+    public virtual void InitSpellDetail() {
+        spellData = new SpellData();
+    }
 
     /// <summary>
     /// 각 플레이어의 스펠 캐스트 컨트롤러의 Update 메소드에서 입력처리 결과에 따라 호출될 메소드.
@@ -23,29 +23,42 @@ public abstract class Spell : MonoBehaviour
     /// <param name="muzzle"> 마법이 발사될 위치 </param>
     public virtual void CastSpell(Transform muzzle)
     {
-        if (castAble)  // inputSystem을 통한 입력 체크는 MagicSpellCastController에서 합니다. 스킬 버튼이 여럿이기 때문.<--- 여기부터 하면 됨   각 스펠들 스크립트, 웨폰컨트롤러.
+        // 해야할 것
+        // 1. 생성 위치
+        // 2. 생성 방향(회전값)
+        // 3. 생성 개수
+
+        if (!spellData.castAble)
         {
-            GameObject spellObject = Instantiate(data.spellObject);
+            spellData.restTime += Time.deltaTime;
+            if (spellData.restTime >= spellData.coolTime)
+            {
+                spellData.castAble = true;
+                spellData.restTime = 0f;
+            }
+        }
+        if (spellData.castAble)  // inputSystem을 통한 입력 체크는 MagicSpellCastController에서 합니다. 스킬 버튼이 여럿이기 때문.<---  각 스펠들 스크립트, 웨폰컨트롤러.
+        {
+            GameObject spellObject = Instantiate(spellData.spellObjectPref);
             spellObject.transform.position = muzzle.position;
 
-            castAble = false;
-        }
-        if (castAble == false)
-        {
-            restTime += Time.deltaTime;
-            if (restTime >= data.coolTime)
-            {
-                castAble = true;
-                restTime = 0f;
-            }
+            spellData.castAble = false;
         }
     }
 
     /// <summary>
-    /// 충돌 처리. 여기서 각 속성에 따른 결과값을 반환한다.     // <---  반환값 void인데 바꿔줘야함.
+    /// 충돌 처리. 여기서 각 속성에 따른 결과값을 반환한다.
     /// </summary>
-    public virtual void CollisionHandling()
+    public virtual SpellData CollisionHandling(SpellData playerSpellData, SpellData opponentsSpellData)
     {
-        
+        switch ()  // <---------------------------------------- 속성계산 식에 따라 처리하는 부분 만들 차례. 여기부터! switch 쓸지는 생각필요!
+        {
+            default:
+                break;
+        }
+
+
+
+        return playerSpellData;
     }
 }
