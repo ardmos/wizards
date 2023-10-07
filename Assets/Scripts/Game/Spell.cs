9b6ac2cc835e2d3376e2ddebd5f6f6d425bf0e1f
ardmos <46891528+ardmos@users.noreply.github.com>
2023-10-08 01:38:28 +0900

    Delete : Spell.cs 필요 없는 코드 삭제
    InitSpellDataDetail()은 각 속성별 마법들(ex. FireSpell.cs)로 이동함.

--- Assets/Scripts/Game/Spell.cs
+++ Assets/Scripts/Game/Spell.cs
@@ -6,7 +6,7 @@ using UnityEngine.EventSystems;
 /// <summary>
 /// 마법의 공통적인 기능들을 관리하는 클래스
 /// </summary>
-public abstract class Spell : MonoBehaviour
+public class Spell : MonoBehaviour
 {
     public enum SpellType
     {
@@ -29,9 +29,4 @@ public abstract class Spell : MonoBehaviour
     public bool castAble;
 
     public GameObject spellObjectPref;
-
-    /// <summary>
-    /// 각 스펠의 디테일값(spellData)은 스펠 본인들이 상속받아서 정의
-    /// </summary>
-    public abstract void InitSpellDataDetail();
 }
