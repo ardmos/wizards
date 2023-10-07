99ae9fba4f6c44775ba2c922b998ce7637902c86
ardmos <46891528+ardmos@users.noreply.github.com>
2023-10-08 01:17:06 +0900

    ADD : Spell Class 코드 추가
    Spell 클래스의 기본 기능을 위한 코드 내용 추가

--- Assets/Scripts/Game/Spell.cs
+++ Assets/Scripts/Game/Spell.cs
@@ -8,5 +8,30 @@ using UnityEngine.EventSystems;
 /// </summary>
 public abstract class Spell : MonoBehaviour
 {
+    public enum SpellType
+    {
+        Fire,
+        Water,
+        Ice,
+        Lightning,
+        Arcane
+    }
+    public SpellType spellType;
 
+    public float coolTime;
+    public float restTime;
+    public float lifeTime;
+    public float moveSpeed;
+    public int damage;
+    public int price;
+    public int level;
+    public string spellName;
+    public bool castAble;
+
+    public GameObject spellObjectPref;
+
+    /// <summary>
+    /// 각 스펠의 디테일값(spellData)은 스펠 본인들이 상속받아서 정의
+    /// </summary>
+    public abstract void InitSpellDataDetail();
 }
