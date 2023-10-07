39d1f5270f26c3565d5804c5c37cf1a8fd147a53
ardmos <46891528+ardmos@users.noreply.github.com>
2023-10-08 01:41:03 +0900

    Add : FireSpell에 코드 추가
    Fire 속성 마법들의 상세 스탯을 각 마법들의 스크립트에서 정의하도록 abstract 형태의 InitiSpellDataDetail() 메소드를 추가.

--- Assets/Scripts/Game/FireSpell.cs
+++ Assets/Scripts/Game/FireSpell.cs
@@ -3,8 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 /// <summary>
 /// 불의 주문들의 공통적인 기능을 관리하는 클래스 
+/// !!! 현재 기능
+/// 1. 상세 속성값은 각 마법스펠 본인들에서 정의하도록 함
 /// </summary>
-public class FireSpell : Spell
+public abstract class FireSpell : Spell
 {
-    
+    /// <summary>
+    /// 상세 속성값은 각 마법스펠 본인들에서 정의
+    /// </summary>
+    public abstract void InitSpellDataDetail();
+
+
 }
