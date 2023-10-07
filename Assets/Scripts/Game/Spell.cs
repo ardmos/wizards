7476fbce19d2ceee6efcf966eb3fe37a9ec2eb75
ardmos <46891528+ardmos@users.noreply.github.com>
2023-10-08 01:15:31 +0900

    DEL : Spell.cs 쓸모없는 코드 삭제
    더 이상 Spell에서 사용되지 않는 내용들 제거

--- Assets/Scripts/Game/Spell.cs
+++ Assets/Scripts/Game/Spell.cs
@@ -8,41 +8,5 @@ using UnityEngine.EventSystems;
 /// </summary>
 public abstract class Spell : MonoBehaviour
 {
-    public SpellData spellData;
 
-    /// <summary>
-    /// 각 스펠의 디테일값(spellData)은 스펠 본인들이 상속받아서 정의
-    /// </summary>
-    public virtual void InitSpellDetail() {
-        spellData = new SpellData();
-    }
-
-    /// <summary>
-    /// 각 플레이어의 스펠 캐스트 컨트롤러의 Update 메소드에서 입력처리 결과에 따라 호출될 메소드.
-    /// </summary>
-    /// <param name="muzzle"> 마법이 발사될 위치 </param>
-    public virtual void CastSpell(Transform muzzle)
-    {
-        // 해야할 것
-        // 1. 생성 위치
-        // 2. 생성 방향(회전값)
-        // 3. 생성 개수
-
-        if (!spellData.castAble)
-        {
-            spellData.restTime += Time.deltaTime;
-            if (spellData.restTime >= spellData.coolTime)
-            {
-                spellData.castAble = true;
-                spellData.restTime = 0f;
-            }
-        }
-        if (spellData.castAble)  // inputSystem을 통한 입력 체크는 MagicSpellCastController에서 합니다. 스킬 버튼이 여럿이기 때문.<---  각 스펠들 스크립트, 웨폰컨트롤러.
-        {
-            GameObject spellObject = Instantiate(spellData.spellObjectPref);
-            spellObject.transform.position = muzzle.position;
-
-            spellData.castAble = false;
-        }
-    }
 }
