12e89515d1bac46a4c6b7216447688e37cc49595
ardmos <46891528+ardmos@users.noreply.github.com>
2023-10-08 00:58:06 +0900

    ADD : FireSpell.cs
    FireSpell 클래스 스크립트 생성

---
+++ Assets/Scripts/Game/FireSpell.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// 불의 주문들의 공통적인 기능을 관리하는 클래스 
+/// </summary>
+public class FireSpell : Spell
+{
+    
+}
