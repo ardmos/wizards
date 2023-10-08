99ed384bb65e93876171d3149f7a36fcc5ed0f86
ardmos <46891528+ardmos@users.noreply.github.com>
2023-10-08 18:39:04 +0900

    Add : FireBall 클래스에 코드 추가
    Level에 따른 FireBall 마법의 상세 능력치를 설정하는 코드를 추가함

--- Assets/Scripts/Game/FireBall.cs
+++ Assets/Scripts/Game/FireBall.cs
@@ -2,19 +2,70 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 /// <summary>
-/// 이곳은 파이어볼 스크립트이고
+/// 이곳은 파이어볼 스크립트입니다.
+/// !!! 현재 기능
+/// 1. Lvl에 따른 상세 능력치 설정
+/// 2. 게임 월드에서의 Spell Object의 이동 처리
+/// 3. CollisionEnter 충돌 처리
 /// </summary>
-public class FireBall : MonoBehaviour
+public class FireBall : FireSpell
 {
+    SpellInfo fireBallLv1;
+    SpellInfo fireBallLv2;
+    SpellInfo fireBallLv3;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        InitSpellInfoDetail();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    /// <summary>
+    /// 1. Lvl에 따른 상세 능력치 설정
+    /// </summary>
+    public override void InitSpellInfoDetail()
+    {
+        fireBallLv1 = new SpellInfo()
+        {
+            spellType = SpellType.Fire,
+            coolTime = 5.0f,
+            restTime = 0.0f,
+            lifeTime = 10.0f,
+            moveSpeed = 10.0f,
+            price = 30,
+            level = 1,
+            spellName = "FireBall Lv.1",
+            castAble = true
+        };
+        fireBallLv2 = new SpellInfo()
+        {
+            spellType = SpellType.Fire,
+            coolTime = 5.0f,
+            restTime = 0.0f,
+            lifeTime = 10.0f,
+            moveSpeed = 10.0f,
+            price = 40,
+            level = 2,
+            spellName = "FireBall Lv.2",
+            castAble = true
+        };
+        fireBallLv3 = new SpellInfo()
+        {
+            spellType = SpellType.Fire,
+            coolTime = 5.0f,
+            restTime = 0.0f,
+            lifeTime = 10.0f,
+            moveSpeed = 10.0f,
+            price = 50,
+            level = 3,
+            spellName = "FireBall Lv.3",
+            castAble = true
+        };
     }
 }
