98e26cb21b2338ca17338ddfc3ade6885c330161
ardmos <46891528+ardmos@users.noreply.github.com>
2023-10-08 13:01:42 +0900

    Add : FireBall.cs 스크립트 추가
    파이어볼 스크립트 생성

---
+++ Assets/Scripts/Game/FireBall.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// 이곳은 파이어볼 스크립트이고
+/// </summary>
+public class FireBall : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
