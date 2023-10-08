a1489f6de01e0b3eba9c38b4dd7c51e2ebfb6784
ardmos <46891528+ardmos@users.noreply.github.com>
2023-10-08 18:20:17 +0900

    Refactor : Spell 클래스 내부의 상세 변수들을 캡슐화
    Spell 내부의 변수들을 SpellInfo 클래스로 캡슐화

--- Assets/Scripts/Game/Spell.cs
+++ Assets/Scripts/Game/Spell.cs
@@ -23,20 +23,22 @@ public abstract class Spell : MonoBehaviour
         Lightning,
         Arcane
     }
-    public SpellType spellType;
+    public class SpellInfo
+    {
+        public SpellType spellType;
 
-    public float coolTime;
-    public float restTime;
-    public float lifeTime;
-    public float moveSpeed;
-    public int damage;
-    public int price;
-    public int level;
-    public string spellName;
-    public bool castAble;
+        public float coolTime;
+        public float restTime;
+        public float lifeTime;
+        public float moveSpeed;
+        public int damage;
+        public int price;
+        public int level;
+        public string spellName;
+        public bool castAble;
 
-    public GameObject spellObjectPref;
-
+        public GameObject spellObjectPref;
+    }
     // 속성별 충돌 계산. 여기선 Lvl와 Type만 반환하고 나머지 속성값은 각 마법스펠에서 입력해 사용한다. (보통은 기존의 본인들 스탯을 그대로 사용하게됨)
     public abstract SpellLvlType CollisionHandling(SpellLvlType thisSpell, SpellLvlType opponentsSpell);
 }
