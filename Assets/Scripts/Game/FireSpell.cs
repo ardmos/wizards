50888f1343d9f67d32f0a0798fe4a7930f9acec6
ardmos <46891528+ardmos@users.noreply.github.com>
2023-10-08 12:58:31 +0900

    Add : FireSpell.cs 에 코드 추가
    불마법 고유의 속성별 충돌 계산을 Spell.cs로부터 상속받아서 구현한 내용을 추가함. (CollisionHadling)

--- Assets/Scripts/Game/FireSpell.cs
+++ Assets/Scripts/Game/FireSpell.cs
@@ -9,24 +9,42 @@ using UnityEngine;
 /// </summary>
 public abstract class FireSpell : Spell
 {
-    public struct CollisionCalcResult
-    {
-        Spell.SpellType spellType;
-        int level;
-    }
-
     /// <summary>
     /// 상세 속성값은 각 마법스펠 본인들에서 정의
     /// </summary>
     public abstract void InitSpellDataDetail();
 
     // 속성별 충돌 계산 구현할 차례. 여기선 Lvl와 Type만 반환하고 나머지 속성값은 현 스크립트를 호출한 각 마법스펠에서 입력해 사용한다. (보통은 기존의 본인들 스탯을 그대로 사용하게됨)
-    public CollisionCalcResult CollisionHandling(Spell thisSpell, Spell opponentsSpell)
+    public override SpellLvlType CollisionHandling(SpellLvlType thisSpell, SpellLvlType opponentsSpell)
     {
-        CollisionCalcResult result = new CollisionCalcResult();
+        SpellLvlType result = new SpellLvlType();
 
-        swit
+        // 1. Lvl 비교
+        int resultLevel = thisSpell.level - opponentsSpell.level;
+        result.level = resultLevel;
+        // 2. resultLevel 값이 0보다 같거나 작으면 더 계산할 필요 없음. 
+        //      0이면 비긴거니까 만들 필요 없고
+        //      마이너스면 진거니까 만들 필요 없음.
+        //      현 메소드를 호출하는 각 마법 스크립트에서는 resultLevel값에 따라 후속 마법 오브젝트 생성여부를 판단하면 됨. 
+        if (resultLevel <= 0)
+        {           
+            return result;
+        }
 
+        // 3. resultLevel값이 0보다 큰 경우는 내가 이긴 경우. 상대 마법 속성별 경우의 수 만을 따져서 결과값을 반환한다.
+        switch (opponentsSpell.spellType)
+        {
+            case SpellType.Fire:
+            case SpellType.Water:
+            case SpellType.Ice:
+            case SpellType.Arcane:
+            default:
+                result.spellType = SpellType.Fire;
+                break;
+            case SpellType.Lightning:
+                result.spellType = SpellType.Arcane;
+                break;
+        }
 
         return result;
     }
