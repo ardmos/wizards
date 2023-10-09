37922cf8042e8271b28c507e62691c86b7e53ff2
ardmos <46891528+ardmos@users.noreply.github.com>
2023-10-09 14:13:25 +0900

    Refactor : FireBall 스크립트를 레벨별로 나눔
    기존에 FireBall 스크립트에서 1,2,3 레벨의 파이어볼을 모두 관리하던 방식에서
    FireBallLv1, 2, 3 세 스크립트로 각각 나뉘어서 관리하는 방식으로 수정.
    FireBall.cs 를 FireBallLv1, FireBallLv2, FireBallLv3 으로 나눔

---
+++ Assets/Scripts/Game/FireBallLv3.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// 
+/// 3레벨 파이어볼 스크립트입니다.
+/// 
+/// !!! 현재 기능
+/// 1. 상세 능력치 설정
+/// 2. 게임 월드에서의 Spell Object의 이동 처리
+/// 3. CollisionEnter 충돌 처리
+/// </summary>
+public class FireBallLv3 : FireSpell
+{
+    SpellInfo spellInfo;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InitSpellInfoDetail();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    /// <summary>
+    /// 1. 상세 능력치 설정
+    /// </summary>
+    public override void InitSpellInfoDetail()
+    {
+        spellInfo = new SpellInfo()
+        {
+            spellType = SpellType.Fire,
+            coolTime = 5.0f,
+            restTime = 0.0f,
+            lifeTime = 10.0f,
+            moveSpeed = 10.0f,
+            price = 50,
+            level = 3,
+            spellName = "FireBall Lv.3",
+            castAble = true
+        };
+    }
+}
