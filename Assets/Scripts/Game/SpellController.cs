10add2e697eb20035caf72dc71849e7635d83584
ardmos <46891528+ardmos@users.noreply.github.com>
2023-10-09 14:14:32 +0900

    Add : SpellController.cs 추가
    캐릭터의 마법 보유 현황 및 버튼 입력 처리를 담당하는 스크립트

---
+++ Assets/Scripts/Game/SpellController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// 플레이어 캐릭터 오브젝트에 붙이는 스크립트
+/// !!! 현재 기능
+/// 1. 마법 부유 현황 관리
+/// 2. 스킬 발동 버튼 입력 처리 
+/// </summary>
+public class SpellController : MonoBehaviour
+{
+    [SerializeField]
+    private Spell currentSpell;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
