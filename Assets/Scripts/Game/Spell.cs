59a963de54e738cd9c8cb91ae2d5fb7305402c7e
ardmos <46891528+ardmos@users.noreply.github.com>
2023-10-09 15:04:25 +0900

    Add : Spell 클래스에 코드 추가
    스킬 시전을 담당하는 virtual 메소드 CastSpell과 마법봉 끝부분 발사 이펙트를 담당하는 MuzzleVFX를 추가함

--- Assets/Scripts/Game/Spell.cs
+++ Assets/Scripts/Game/Spell.cs
@@ -37,4 +37,26 @@ public abstract class Spell : MonoBehaviour
     }
     // 속성별 충돌 계산. 여기선 Lvl와 Type만 반환하고 나머지 속성값은 각 마법스펠에서 입력해 사용한다. (보통은 기존의 본인들 스탯을 그대로 사용하게됨)
     public abstract SpellLvlType CollisionHandling(SpellLvlType thisSpell, SpellLvlType opponentsSpell);
+
+    public virtual void CastSpell(Transform muzzle)
+    {
+
+    }
+
+    public virtual void MuzzleVFX(GameObject muzzlePrefab, Transform muzzle)
+    {
+        if (muzzlePrefab != null)
+        {
+            var muzzleVFX = Instantiate(muzzlePrefab, muzzle.position, Quaternion.identity);
+            muzzleVFX.transform.forward = gameObject.transform.forward;
+            var ps = muzzleVFX.GetComponent<ParticleSystem>();
+            if (ps != null)
+                Destroy(muzzleVFX, ps.main.duration);
+            else
+            {
+                var psChild = muzzleVFX.transform.GetChild(0).GetComponent<ParticleSystem>();
+                Destroy(muzzleVFX, psChild.main.duration);
+            }
+        }
+    }
 }
