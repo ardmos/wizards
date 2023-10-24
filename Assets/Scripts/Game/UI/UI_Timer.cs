6039b9696e3f90a2d21d57f76b72165811a3ddd4
ardmos <46891528+ardmos@users.noreply.github.com>
2023-10-24 23:37:54 +0900

    Feat : GameScene 상단 타이머 UI 기능 구현

---
+++ Assets/Scripts/Game/UI/UI_Timer.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class UI_Timer : MonoBehaviour
+{
+    [SerializeField] private bool isTimerOn = false;
+    [SerializeField] private TextMeshProUGUI txtTimer;
+    private float totalPassedTime;
+    int h, m, s;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // 
+        StartTimer();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isTimerOn)
+        {
+            totalPassedTime += Time.deltaTime;
+            h = ((int)totalPassedTime / 3600);
+            m = ((int)totalPassedTime / 60 % 60);
+            s = ((int)totalPassedTime % 60);
+            txtTimer.text = $"{h, 2:D2}:{m, 2:D2}:{s, 02:D2}";
+        }
+    }
+
+    public void StartTimer()
+    {
+        isTimerOn = true;
+    }
+    
+}
