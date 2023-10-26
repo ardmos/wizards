fb7eb32999b7dc2dd127532b268591d4ad1e4785
ardmos <46891528+ardmos@users.noreply.github.com>
2023-10-26 20:16:28 +0900

    Add : GameManager에 기본 StateMachine 코드 추가

--- Assets/Scripts/Game/GameManager.cs
+++ Assets/Scripts/Game/GameManager.cs
@@ -1,10 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-// 여기부터. 게임 진행 흐름 만들 차례.
+
+/// <summary>
+/// Singleton
+/// EventSystem(Observer Pattern)
+/// Game State Machine
+/// ownerPlayerObject
+/// </summary>
+
 public class GameManager : MonoBehaviour
 {
+    public static GameManager Instance { get; private set; }
+
+    private enum State
+    {
+        WatingToStart,
+        CountdownToStart,
+        GamePlaying,
+        GameOver,
+    }
+
+    private State state;
+    private float watingToStartTimer = 1f;
+    private float countdownToStartTimer = 3f;
+    private float gamePlayingTimer = 1f;
+
     public GameObject ownerPlayerObject { get; set; }
+
+    void Awake()
+    {
+        Instance = this;
+        state = State.WatingToStart;
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -15,6 +43,40 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        RunStateMachine();
     }
+
+    private void RunStateMachine()
+    {
+        switch (state)
+        {
+            case State.WatingToStart:
+                watingToStartTimer -= Time.deltaTime;
+                if (watingToStartTimer < 0f)
+                {
+                    state = State.CountdownToStart;
+                }
+                break;
+            case State.CountdownToStart:
+                countdownToStartTimer -= Time.deltaTime;
+                if (countdownToStartTimer < 0f)
+                {
+                    state = State.GamePlaying;
+                }
+                break;
+            case State.GamePlaying:
+                gamePlayingTimer -= Time.deltaTime;
+                if (gamePlayingTimer < 0f)
+                {
+                    state = State.GameOver;
+                }
+                break;
+            case State.GameOver:
+                break;
+            default:
+                break;
+        }
+        Debug.Log(state);
+    }
+
 }
