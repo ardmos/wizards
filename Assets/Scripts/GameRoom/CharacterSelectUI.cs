e2973f0b4ce914253518cf86f94fc653955f39f7
ardmos <46891528+ardmos@users.noreply.github.com>
2023-11-01 02:13:14 +0900

    Test : GameRoom 씬 생성
    Ready 버튼만 만들어둠.
    해당 버튼 기능 구현부터 하면 됨!
    GameScene에 만들어둔 ready 기능 참고

---
+++ Assets/Scripts/GameRoom/CharacterSelectUI.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterSelectUI : MonoBehaviour
+{
+    [SerializeField] private Button btnReady;
+}
