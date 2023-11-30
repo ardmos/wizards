98004dd6ad3094a631429427b41ed30082b2eabd
ardmos <46891528+ardmos@users.noreply.github.com>
2023-11-30 19:57:36 +0900

    Feat : 설정팝업의 SFX바 UI On/Off 기능 추가
    슬라이더 값에 따라 UI 변경

--- Assets/Scripts/Lobby/PopupSettings/SoundGroupUI.cs
+++ Assets/Scripts/Lobby/PopupSettings/SoundGroupUI.cs
@@ -1,12 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 /// <summary>
-///  
-/// 볼륨없으면ㅇ 미ㅣ지 교체 
+/// SoundManager와 연계 
+/// 볼륨없으면 아이콘 이미지 교체 
 /// </summary>
 public class SoundGroupUI : MonoBehaviour
 {
+    [SerializeField] private Image icon;
+    [SerializeField] private Image handle;
+    [SerializeField] private Slider slider;
+    [SerializeField] private Sprite iconSoundOn;
+    [SerializeField] private Sprite iconSoundOff;
+    [SerializeField] private Sprite handleSoundOn;
+    [SerializeField] private Sprite handleSoundOff;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,6 +25,17 @@ public class SoundGroupUI : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        UpdateIconImage();
+    }
+
+    private void UpdateIconImage()
+    {
+        if (slider.value != 0) { 
+            icon.sprite = iconSoundOn;
+            handle.sprite = handleSoundOn;
+            return; 
+        }
+        icon.sprite = iconSoundOff;
+        handle.sprite = handleSoundOff;
     }
 }
