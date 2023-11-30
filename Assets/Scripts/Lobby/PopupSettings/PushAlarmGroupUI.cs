a0a28e23b55b52fe06f4edb3e2dce156680f00cd
ardmos <46891528+ardmos@users.noreply.github.com>
2023-11-30 20:12:44 +0900

    Add : Push알람 설정 UI 컨트롤러 스크립트 추가.
    구현 여부는 추후 기획에 따라 결정할것.

---
+++ Assets/Scripts/Lobby/PopupSettings/PushAlarmGroupUI.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 푸쉬알람 관리. Firebase API와 연계해서 구현해야할듯. 기획에 따라 구현여부 결정하기. 
+/// </summary>
+
+public class PushAlarmGroupUI : MonoBehaviour
+{
+    [SerializeField] private SwitchUI switchUI;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Lobby/PopupSettings/SwitchUI.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// 1. 스위치 이미지 변경
+/// 2. 스위치 상태 업데이트
+/// </summary>
+public class SwitchUI : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
