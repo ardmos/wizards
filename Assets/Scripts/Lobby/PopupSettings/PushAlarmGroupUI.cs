1bed7466a1d6865cfb8a777de9b7128dabd797ed
ardmos <46891528+ardmos@users.noreply.github.com>
2023-12-02 02:43:53 +0900

    Feat : Lobby씬 Setting 팝업 토글UI 기능 구현 완료

--- Assets/Scripts/Lobby/PopupSettings/PushAlarmGroupUI.cs
+++ Assets/Scripts/Lobby/PopupSettings/PushAlarmGroupUI.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 /// <summary>
@@ -9,15 +7,4 @@ using UnityEngine;
 public class PushAlarmGroupUI : MonoBehaviour
 {
     [SerializeField] private SwitchUI switchUI;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
