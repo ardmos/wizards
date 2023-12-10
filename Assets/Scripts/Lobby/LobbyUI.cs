c8eff3fa41e15d5c2cd2436e497418ba486ff187
ardmos <46891528+ardmos@users.noreply.github.com>
2023-12-10 13:57:52 +0900

    Add : Lobby씬 UI팝업들 추가
    Equipment, Clan, Mod 팝업 추가

--- Assets/Scripts/Lobby/LobbyUI.cs
+++ Assets/Scripts/Lobby/LobbyUI.cs
@@ -8,10 +8,17 @@ public class LobbyUI : MonoBehaviour
     [SerializeField] private Button btnUserInfo;
     [SerializeField] private Button btnSettings;
     [SerializeField] private Button btnShop;
+    [SerializeField] private Button btnEquipment;
+    [SerializeField] private Button btnSkill;
+    [SerializeField] private Button btnClan;
+    [SerializeField] private Button btnMod;
 
     [SerializeField] private PopupUserInfoUI popupUserInfoUI;
     [SerializeField] private PopupSettingsUI popupSettingsUI;
     [SerializeField] private PopupShopUI popupShop;
+    [SerializeField] private PopupEquipmentUI popupEquipment;
+    [SerializeField] private PopupClanUI popupClan;
+    [SerializeField] private PopupModUI popupMod;
 
     // Start is called before the first frame update
     void Start()
@@ -19,6 +26,9 @@ public class LobbyUI : MonoBehaviour
         btnUserInfo.onClick.AddListener(() => { popupUserInfoUI.Show(); });
         btnSettings.onClick.AddListener(() => { popupSettingsUI.Show(); });
         btnShop.onClick.AddListener(() => { popupShop.Show(); });
+        btnEquipment.onClick.AddListener(() => { popupEquipment.Show(); });
+        btnClan.onClick.AddListener(() => { popupClan.Show(); });
+        btnMod.onClick.AddListener(() => { popupMod.Show(); });
     }
 
     // Update is called once per frame
---
+++ Assets/Scripts/Lobby/PopupModUI.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PopupModUI : MonoBehaviour
+{
+    [SerializeField] private Button btnClose;
+    [SerializeField] private Button btnHome;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        btnClose.onClick.AddListener(Hide);
+        btnHome.onClick.AddListener(Hide);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void Show()
+    {
+        gameObject.SetActive(true);
+    }
+
+    private void Hide()
+    {
+        gameObject.SetActive(false);
+    }
+}
