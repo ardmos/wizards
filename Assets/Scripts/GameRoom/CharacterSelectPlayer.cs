021c31465fe0a91762d134e1b39d834285b51332
ardmos <46891528+ardmos@users.noreply.github.com>
2023-12-17 01:43:06 +0900

    Delete : GameRoomCharacterPrefab.cs 삭제.
    해당 스크립트의 역할을 CharacterSelectPlayer.cs로 통합됨.

--- Assets/Scripts/GameRoom/CharacterSelectPlayer.cs
+++ Assets/Scripts/GameRoom/CharacterSelectPlayer.cs
@@ -7,8 +7,14 @@ public class CharacterSelectPlayer : MonoBehaviour
 {
     [SerializeField] private int playerIndex;
     [SerializeField] private GameObject readyGameObject;    
-    public GameRoomCharacterPrefab gameRoomCharacterPrefab;
 
+    /// <summary>
+    /// GameRoom씬에서 사용되는 Player 캐릭터 프리팹의 스크립트로
+    /// 각 Player들이 선택한 영웅의 3D Visual 프리팹을 자식오브젝트로 가져와 보여주는 역할을 한다. 
+    /// 1. Player들이 선택한 영웅의 Prefab 가져와 Prefab을 자식 오브젝트로 인스턴시에이트 하기
+    /// </summary>
+    // 캐릭터 보여주기
+    [SerializeField] private GameObject playerObject;
 
     private void Start()
     {
@@ -65,5 +71,27 @@ public class CharacterSelectPlayer : MonoBehaviour
     {
         gameObject.SetActive(false);
         gameRoomCharacterPrefab.HideCharacter3DVisual();
+    }
+
+    // 캐릭터 보여주기
+    /// <summary>
+    /// Player가 선택한 캐릭터의 비주얼을 가져와 보여줍니다.
+    /// 총 과정 
+    /// 1. 로비씬에서 PlayerProfileData에 선택중인 클래스를 저장
+    /// 2. 서버가 열리면 GameMultiplayer의 NetworkList인 playerDataList에 저장.
+    /// 3. 필요할 때 playerIndex나 clientID로 꺼내서 사용.
+    /// </summary>
+    public void ShowCharacter3DVisual(int playerIndex)
+    {
+        //Debug.Log($"LoadCharacter3DVisual playerIndex:{playerIndex}, playerObject:{playerObject}");
+        playerObject = Instantiate(GameMultiplayer.Instance.GetPlayerClassPrefabByPlayerIndex_NotForGameSceneObject(playerIndex));
+        playerObject.transform.SetParent(transform, false);
+        playerObject.transform.localPosition = Vector3.zero;
+    }
+
+    public void HideCharacter3DVisual()
+    {
+        Destroy(playerObject);
+        playerObject = null;
     }
 }
