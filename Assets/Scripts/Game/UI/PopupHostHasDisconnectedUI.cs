e2937c133b00a5ad97cc90f6c2b998c128878566
ardmos <46891528+ardmos@users.noreply.github.com>
2023-12-19 17:11:19 +0900

    Fix : GameRoom Client 퇴장시 발생 에러 수정
    서버측 연결이 끊겼을 시 노출되는 UI 컨트롤러 스크립트에서,
    EventHanler 구독 해제 시점이 잘못되어있었음.

--- Assets/Scripts/Game/UI/PopupHostHasDisconnectedUI.cs
+++ Assets/Scripts/Game/UI/PopupHostHasDisconnectedUI.cs
@@ -10,29 +10,28 @@ public class PopupHostHasDisconnectedUI : MonoBehaviour
     {
         btnPlayAgain.onClick.AddListener(() =>
         {
-            NetworkManager.Singleton.Shutdown();
-            // 로비로 이동 전에 NetworkManager, GameMultiplayManager 중복되지 않도록 깔끔하게 정리.
-            CleanUp();
+            NetworkManager.Singleton.Shutdown();           
             // 로비로 이동.
             LoadingSceneManager.Load(LoadingSceneManager.Scene.LobbyScene);
         });
+        NetworkManager.Singleton.OnClientDisconnectCallback += NetworkManager_OnClientDisconnectCallback;
     }
 
     private void Start()
     {
-        NetworkManager.Singleton.OnClientDisconnectCallback += NetworkManager_OnClientDisconnectCallback;
         Hide();              
     }
 
     private void OnDestroy()
-    {
-        // 강퇴당했을 시 NetworkManager와 현 스크립트의 오브젝트는 라이프사이클이 다르기 때문에 손수 이벤트 구독을 해제해준다
+    {        
+        //NetworkManager.Singleton.OnClientDisconnectCallback
         NetworkManager.Singleton.OnClientDisconnectCallback -= NetworkManager_OnClientDisconnectCallback;
+        CleanUp();
     }
 
     private void NetworkManager_OnClientDisconnectCallback(ulong clientId)
     {
-        //Debug.Log($"누군가 나갔습니다. clientId : {clientId}");
+        // 연결을 끊은것이 서버일 경우
         if (clientId == NetworkManager.ServerClientId)
         {
             // Server is shutting down
