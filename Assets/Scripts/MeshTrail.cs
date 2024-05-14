b8499f6a5340d5db1aa5f49fa2455e74070e097f
ardmos <46891528+ardmos@users.noreply.github.com>
2024-05-15 00:53:28 +0900

    Refactor : Mesh Trail 클래스에 ObjectPooling 적용

--- Assets/Scripts/MeshTrail.cs
+++ Assets/Scripts/MeshTrail.cs
@@ -5,9 +5,9 @@ using UnityEngine;
 public class MeshTrail : MonoBehaviour
 {
     [Header("Mesh Related")]
-    public float activeTime = 3f;
+    public float activeTime = 0.1f;
     public float meshRefreshRate = 0.1f;
-    public float meshDestroyDelay = 3f;
+    public float meshDestroyDelay = 0.1f;
     public Transform positionToSpawn;
 
     [Header("Shader Related")]
@@ -16,49 +16,85 @@ public class MeshTrail : MonoBehaviour
     public float shaderVarRate = 0.1f;
     public float shaderVarRefreshRate = 0.05f;
 
+    [SerializeField] private bool activateTrail;
     [SerializeField] private bool isTrailActive;
     [SerializeField] private SkinnedMeshRenderer[] skinnedMeshRenderers;
+
+    // 오브젝트 풀링
+    [SerializeField] private ObjectPool objectPool;
 
     // Update is called once per frame
     void Update()
     {
-        if (!isTrailActive)
+        if (activateTrail && !isTrailActive)
         {
             isTrailActive = true;
             StartCoroutine(ActivateTrail(activeTime));
         }
     }
 
+    public void ActivateTrail()
+    {
+        activateTrail = true;
+    }
+
     private IEnumerator ActivateTrail(float timeActive)
     {
+        // Debug.Log("Start");
         while (timeActive > 0)
         {
             timeActive -= meshRefreshRate;
+            //Debug.Log($"timeActive: {timeActive}");
 
             for (int i=0; i<skinnedMeshRenderers.Length; i++)
             {
-                // 추후 오브젝트 풀링으로 전환해야합니다.
-                GameObject gameObject = new GameObject();
+                // 오브젝트 풀링 사용
+                GameObject gameObject = objectPool.GetObject();
+                //GameObject gameObject = new GameObject();
+                if (gameObject == null)
+                {
+                    Debug.LogWarning("Failed to get object from object pool. Returning.");
+                    yield break;
+                }
+                // 위치와 회전 설정
                 gameObject.transform.SetPositionAndRotation(positionToSpawn.position, positionToSpawn.rotation);
 
-                MeshRenderer meshRenderer =  gameObject.AddComponent<MeshRenderer>();
-                MeshFilter meshFilter = gameObject.AddComponent<MeshFilter>();
 
-                Mesh mesh = new Mesh();
-                skinnedMeshRenderers[i].BakeMesh(mesh, meshFilter);
+                MeshRenderer meshRenderer;
+                MeshFilter meshFilter;
+                if (!gameObject.GetComponent<MeshRenderer>() || !gameObject.GetComponent<MeshFilter>())
+                {
+                    // Mesh 생성
+                    meshRenderer = gameObject.AddComponent<MeshRenderer>();
+                    meshFilter = gameObject.AddComponent<MeshFilter>();
 
-                meshFilter.mesh = mesh;
-                meshRenderer.material = material;
+                    Mesh mesh = new Mesh();
+                    skinnedMeshRenderers[i].BakeMesh(mesh, meshFilter);
+                    meshFilter.mesh = mesh;
+                    meshRenderer.material = material;
+                }
+                else
+                {
+                    // 이미 컴포넌트가 있는 경우 존재하는 컴포넌트를 가져오기만 한다
+                    meshRenderer = gameObject.GetComponent<MeshRenderer>();
+                }
 
+                // Material 및 애니메이션 설정
                 StartCoroutine(AnimateMaterialFloat(meshRenderer.material, 0, shaderVarRate, shaderVarRefreshRate));
 
-                Destroy(gameObject, meshDestroyDelay);
+                // 일정시간 이후 오브젝트풀에 오브젝트 반환
+                // 반환 전에 alpha값 원상복구
+                meshRenderer.material.SetFloat(shaderVarRef, 1f);
+                StartCoroutine(ReturnObjectDelayed(gameObject, meshDestroyDelay));
+                //Destroy(gameObject, meshDestroyDelay);
             }
 
             yield return new WaitForSeconds(meshRefreshRate);
         }
 
         isTrailActive = false;
+        activateTrail = false;
+        //Debug.Log("End");
     }
 
     private IEnumerator AnimateMaterialFloat(Material material, float goal, float rate, float refreshRate)
@@ -71,6 +107,12 @@ public class MeshTrail : MonoBehaviour
             material.SetFloat(shaderVarRef, valueToAnimate);
             yield return new WaitForSeconds(refreshRate);
         }
+    }
+
+    private IEnumerator ReturnObjectDelayed(GameObject obj, float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        objectPool.ReturnObject(obj);
     }
 
 }
