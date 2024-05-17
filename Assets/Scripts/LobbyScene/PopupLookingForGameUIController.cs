faa2297922b667fd454c443d0ade5c46d343f191
ardmos <46891528+ardmos@users.noreply.github.com>
2024-05-18 01:28:54 +0900

    Design : Lobby씬 매칭 찾는 UI 애니메이션 추가
    Popup Looking For Game에 텍스트 애니메이션 효과 추가

--- Assets/Scripts/LobbyScene/PopupLookingForGameUIController.cs
+++ Assets/Scripts/LobbyScene/PopupLookingForGameUIController.cs
@@ -1,16 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class PopupLookingForGameUIController : MonoBehaviour
 {
+    public TextMeshProUGUI textMeshProUGUI;
+
+    private WaitForSeconds waitForSeconds = new WaitForSeconds(1);
+    [SerializeField] private string[] texts;
+
     public void Show()
-    {
+    { 
         gameObject.SetActive(true);
+        StartCoroutine(TextAnimation());
     }
 
     public void Hide()
     {
         gameObject.SetActive(false);
+        StopCoroutine(TextAnimation());
+    }
+
+    private IEnumerator TextAnimation()
+    {
+        while (true)
+        {
+            for(int i = 0; i < texts.Length; i++)
+            {
+                textMeshProUGUI.text = texts[i];
+                yield return waitForSeconds;
+            }
+        }
     }
 }
