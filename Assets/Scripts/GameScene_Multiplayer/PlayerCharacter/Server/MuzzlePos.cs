c3e7a83ff944492fca8a16ddd23cdf07a748bbe5
ardmos <46891528+ardmos@users.noreply.github.com>
2024-11-07 17:43:39 +0900

    Dir : PlayerCharacter 폴더 내 디렉토리 정리
    한 일 : PlayerCharacter 폴더 내 Server/Client 기준으로 분류되지 않은 스크립트들 각 폴더로 이동
    목적 : 프로젝트 유지보수성 향상

--- Assets/Scripts/GameScene_Multiplayer/PlayerCharacter/Server/MuzzlePos.cs
+++ Assets/Scripts/GameScene_Multiplayer/PlayerCharacter/Server/MuzzlePos.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 /// <summary>
 /// SpellManager에서 마법을 시전할 위치를 찾을 때 사용하는 스크립트 입니다.
-/// 이제[ 사용 안합니다. 삭제 가능합니다.
+/// GetComponentInChildren을 사용하는것도 성능면에서 손해고 하니 이 컴포넌트를 사용하지 않는 방향으로 수정하는게 좋아보입니다.
 /// </summary>
 public class MuzzlePos : MonoBehaviour
 {
